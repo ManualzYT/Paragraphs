57e7fb02182d04cf3d05e13ff61362693a0a9234
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-03-30 16:13:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+functoin getParagraph1()
+{
+var inputs = [] ;
+}
+
+for(var i = 1 ; i <=6 ; i++)    
+{
+    inputs.push(document.getElementById("para1_input_box_" + i).value);
+}
+
+inputs.join(". ");
