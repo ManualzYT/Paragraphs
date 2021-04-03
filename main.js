56ed25f6eaacfafef7225beac059e952dfaebf7b
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-04-03 13:31:26 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-functoin getParagraph1()
+function getParagraph1()
 {
-var inputs = [] ;
-}
-
-for(var i = 1 ; i <=6 ; i++)    
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++)
+    
 {
     inputs.push(document.getElementById("para1_input_box_" + i).value);
 }
+  document.getElementById("showParagraph1").innerHTMl = inputs.join(". ")
 
 inputs.join(". ");
+document.getElementById("showParagraph1").innerHTML = inputs.join(". ")
+}
