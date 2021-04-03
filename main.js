d18bec64b6a2aa245ac3d92d9cab07d54df54ab9
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-04-03 13:46:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ for(var i = 1 ; i <=6 ; i++)
 {
     inputs.push(document.getElementById("para1_input_box_" + i).value);
 }
-  document.getElementById("showParagraph1").innerHTMl = inputs.join(". ")
-
-inputs.join(". ");
-document.getElementById("showParagraph1").innerHTML = inputs.join(". ")
+  document.getElementById("getParagraph1").innerHTMl = inputs.join(". ")
 }
+inputs.join("text_input_1 , text_input_2 , text_input_3 , text_input_4 , text_input_5 , text_input_6");
+document.getElementById("getParagraph1").innerHTML = inputs.join("text_input_1 , text_input_2 , text_input_3 , text_input_4 , text_input_5 , text_input_6 ");
